2dbec728cb386b06efa37d2b4d6b156ad8fd386a
justanotheratom <sanketpatel.1805@gmail.com>
2025-03-27 13:03:48 -0700

    feat: add cache control headers for static assets

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/speed-insights/script.js',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=2678400, must-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/insights/script.js',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=2678400, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
